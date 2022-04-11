718227aad4d7f43239babc98150355d76a9370d9
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-04-11 15:54:35 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// MAIL
+// Creare una mail list di email che possono accedere.
+// Chiedere la mail all'uttente.
+// Controllare se la mail inserita dall'uttente sia presente nella mail list.
+// Creare i casi in cui sia presente o no.
+
+const mailList = ["violinhi@hotmail.com", 
+  "aardo@hotmail.com", 
+  "smeier@outlook.com", 
+  "sburke@outlook.com", 
+  "cameron@verizon.net", 
+  "phyruxus@live.com", 
+  "mugwump@msn.com", 
+  "sarahs@yahoo.ca", 
+  "shawnce@optonline.net", 
+  "jesse@mac.com", 
+  "purvis@msn.com", 
+  "rkobes@mac.com"
+]
+
+
+const userMail = "jesse@mac.com" /* prompt("Inserire eMail") */;
+
+for(let i = 0; i < mailList.length; i++){
+  
+  if(userMail === mailList[i]){
+    
+  }
+
+}
